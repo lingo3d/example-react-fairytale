be6fa515cec1e7d394f8266dc23841609b3812a1
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-07-15 04:20:19 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,93 @@
-import { Joystick, Model, Skybox, ThirdPersonCamera, useKeyboard, useLoop, World, usePreload, useWindowSize } from "lingo3d-react"
-import { useRef, useState } from "react"
-import "./App.css"
+import {
+  Joystick,
+  Model,
+  ThirdPersonCamera,
+  World,
+  usePreload,
+  useWindowSize,
+  Dummy,
+  keyboard,
+} from "lingo3d-react";
+import { useRef, useEffect } from "react";
+import "./App.css";
 
 //Main game component that's rendered after loading is done
 const Game = () => {
-
-  //useKeyboard hook will trigger re-render when the user presses keyboard
-  const key = useKeyboard()
-
-  //joystick coordinates
-  const [joystick, setJoystick] = useState({ x: 0, y: 0, angle: 0 })
-
-  //"running" becomes true when user presses "w" key, or pushes joystick forward
-  const running = key === "w" || joystick.y < 0
+  //adjust FOV based on portrait or landscape orientation
+  const windowSize = useWindowSize();
+  const fov = windowSize.width > windowSize.height ? 90 : 120;
 
   //ref to the character model, used to imperatively move the character forward
-  const characterRef = useRef()
+  const characterRef = useRef();
 
-  //Callback passed to useLoop runs 60 times every second. When "running" is false, loop is paused automatically.
-  useLoop(() => {
-    characterRef.current.moveForward(-6)
-  }, running)
+  useEffect(() => {
+    keyboard.onKeyPress = (_, keys) => {
+      const character = characterRef.current;
+      if (!character) return;
 
-  //adjust FOV based on portrait or landscape orientation
-  const windowSize = useWindowSize()
-  const fov = windowSize.width > windowSize.height ? 75 : 90
+      if (keys.has("w")) character.strideForward = -5;
+      else if (keys.has("s")) character.strideForward = 5;
+      else character.strideForward = 0;
+
+      if (keys.has("a")) character.strideRight = 5;
+      else if (keys.has("d")) character.strideRight = -5;
+      else character.strideRight = 0;
+    };
+  }, []);
 
   return (
-    <>
-      <World>
-        {/* map model */}
-        <Model src="fairy.glb" scale={20} physics="map" />
-        {/* camera and player character model */}
-        <ThirdPersonCamera active mouseControl fov={fov}>
-          <Model
-            ref={characterRef}
-            src="person.glb"
-            y={200}
-            z={-150}
-            animations={{ idleAnimation: "idle.fbx", runningAnimation: "running.fbx" }}
-            animation={ running ? "runningAnimation" : "idleAnimation" }
-            physics="character"
-          />
-        </ThirdPersonCamera>
-        <Skybox texture="skybox.jpg" />
-      </World>
-      <Joystick onMove={e => setJoystick({ x: e.x, y: e.y, angle: e.angle })} />
-    </>
-  )
-}
+    <World skybox="skybox.jpg">
+      {/* map model */}
+      <Model src="fairy.glb" scale={30} physics="map" />
+      {/* camera and player character model */}
+      <ThirdPersonCamera
+        active
+        mouseControl
+        fov={fov}
+        lockTargetRotation="dynamic-lock"
+      >
+        <Dummy
+          ref={characterRef}
+          src="person.glb"
+          y={200}
+          z={-150}
+          physics="character"
+          strideMove
+        />
+      </ThirdPersonCamera>
+      {/* joystick */}
+      {/* 摇杆 */}
+      <Joystick
+        onMove={(e) => {
+          const character = characterRef.current;
+          if (!character) return;
+
+          character.strideForward = -e.y * 5;
+          character.strideRight = -e.x * 5;
+        }}
+        onMoveEnd={() => {
+          const character = characterRef.current;
+          if (!character) return;
+
+          character.strideForward = 0;
+          character.strideRight = 0;
+        }}
+      />
+    </World>
+  );
+};
 
 const App = () => {
   //usePreload hook to preload assets, returns loading progress
-  const progress = usePreload(["fairy.glb", "idle.fbx", "person.glb", "running.fbx", "skybox.jpg"], "21.2mb")
+  const progress = usePreload(
+    ["fairy.glb", "person.glb", "skybox.jpg"],
+    "21.2mb"
+  );
 
   if (progress < 100)
-    return (
-      <div className="loading">
-        loading {Math.round(progress)}%
-      </div>
-    )
+    return <div className="loading">loading {Math.round(progress)}%</div>;
 
-  return (
-    <Game />
-  )
-}
+  return <Game />;
+};
 
-export default App
+export default App;
